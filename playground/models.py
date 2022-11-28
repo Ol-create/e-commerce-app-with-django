da9ebfd2e3cceb33615a969e55f869913129541f
Oluyemi Paul <oluola96@gmail,com>
2022-11-28 23:55:24 +0100

    add product model class

--- playground/models.py
+++ playground/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField();
+    prices = models.DecimalField(max_digits=6, decimal_places=2)
