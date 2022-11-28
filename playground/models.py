8991a6f34ed2c80e9aab13b67f9c07de533b7de4
Oluyemi Paul <oluola96@gmail,com>
2022-11-29 00:00:02 +0100

    add last_update and invetory field to product model

--- playground/models.py
+++ playground/models.py
@@ -4,3 +4,5 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField();
     prices = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
